298e0f48c6ae35b5a12a78e5f45a472dd0d2520b
Lesner <Daniel.Lesner@gfk.com>
2020-09-19 00:28:24 +0300

    Add favico & head meta and rename some classes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let activeCategory = 'home'
-let activeCVCategory = 'cv__objective'
+let activeCategory = 'main__home'
+let activeCVCategory = 'main__cv__objective'
 let cvNavBarOpen = false
 
 const showCategory = (category) => {
@@ -8,8 +8,8 @@ const showCategory = (category) => {
     document.getElementById('navbar__burger').checked = false;
     activeCategory = category;
     
-    if (category == 'cv') {
-        showCVCategory('cv__objective')
+    if (category == 'main__cv') {
+        showCVCategory('main__cv__objective')
     }
     
     if (window.innerWidth < 800) {
@@ -23,8 +23,8 @@ const showCVCategory = (category) => {
         document.getElementById(category).style.display = 'block';
         closeCVNavBar()
     } else {
-        document.getElementById('cv__navbar').style.width = "20%";
-        document.getElementById('cv__navbar').style.display ="inline-block";
+        document.getElementById('main__cv__navbar').style.width = "20%";
+        document.getElementById('main__cv__navbar').style.display ="inline-block";
         document.getElementById(activeCVCategory).style.display = 'none';
         document.getElementById(category).style.display = 'block';
     }
@@ -38,11 +38,11 @@ const showCVNavBar = () => {
 
 const closeCVNavBar = () => {
     cvNavBarOpen = !cvNavBarOpen
-    document.getElementById('cv__navbar__arrow').style.borderLeft = '20px solid green'
-    document.getElementById('cv__navbar__arrow').style.left = '5%'
-    document.getElementById('cv__navbar__arrow').style.transform = 'rotate(0deg)'
-    document.getElementById('cv__navbar').style.width = '5%'
-    items = document.getElementsByClassName('cv__navbar__item')
+    document.getElementById('main__cv__navbar__arrow').style.borderLeft = '20px solid green'
+    document.getElementById('main__cv__navbar__arrow').style.left = '5%'
+    document.getElementById('main__cv__navbar__arrow').style.transform = 'rotate(0deg)'
+    document.getElementById('main__cv__navbar').style.width = '5%'
+    items = document.getElementsByClassName('main__cv__navbar__item')
 
     for (let eachItem of items) {
         eachItem.style.display = 'none'
@@ -51,11 +51,11 @@ const closeCVNavBar = () => {
 
 const openCVNavBar = () => {
     cvNavBarOpen = !cvNavBarOpen
-    document.getElementById('cv__navbar__arrow').style.borderLeft = '20px solid red'
-    document.getElementById('cv__navbar__arrow').style.left = '30%'
-    document.getElementById('cv__navbar__arrow').style.transform = 'rotate(180deg)'
-    document.getElementById('cv__navbar').style.width = '30%'
-    items = document.getElementsByClassName('cv__navbar__item')
+    document.getElementById('main__cv__navbar__arrow').style.borderLeft = '20px solid red'
+    document.getElementById('main__cv__navbar__arrow').style.left = '30%'
+    document.getElementById('main__cv__navbar__arrow').style.transform = 'rotate(180deg)'
+    document.getElementById('main__cv__navbar').style.width = '30%'
+    items = document.getElementsByClassName('main__cv__navbar__item')
 
     for (let eachItem of items) {
         eachItem.style.display = 'block'
